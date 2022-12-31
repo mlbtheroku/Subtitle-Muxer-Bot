422913f1c7f3717f38e828aebdc1cc488ad88fe9
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-12-31 18:08:51 +0630

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', "5941906344:AAFpx76Y1aIthLCQMTteqcQbntQ6-NEvwnA")
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5941906344:AAFpx76Y1aIthLCQMTteqcQbntQ6-NEvwnA')
     APP_ID = os.environ.get('APP_ID', 15830858)
-    API_HASH = os.environ.get('API_HASH', "2c015c994c57b312708fecc8a2a0f1a6")
+    API_HASH = os.environ.get('API_HASH', '2c015c994c57b312708fecc8a2a0f1a6')
 
     #comma seperated user id of users who are allowed to use
 
